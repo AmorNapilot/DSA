a36f33537bb6c0a60bd2d6d1562360cfbda9fd20
AmorNapilot <noellaamor.napilot@gmail.com>
2016-10-03 05:55:52 -0700

    Create MinMax.java

---
+++ Find-Min-Max/MinMax.java
@@ -0,0 +1,16 @@
+public class MinMax
+{
+    public static void main(String[] args) {
+        int [] numbers = {88, 33, 55, 23, 64, 123};
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < numbers.length;i++){
+           if (min > numbers[i])
+           min = numbers[i];
+           if (numbers[i] > max)
+            max = numbers[i];
+        }
+        System.out.println("The smallest number is " + min);
+        System.out.println("The largest number is " + max);
+    }
+}
